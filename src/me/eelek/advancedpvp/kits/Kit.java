d481e8d67275ad6d33606afb4f7e6bf8ead125c5
Eelek <eelke.spijk@gmail.com>
2019-05-01 18:10:34 +0200

    Revert "Revert "AdvancedPvP version 2.0: The Inventory Update""
    This reverts commit fb188d4acc80f3ba6b7d061bf57d508cade7f4a5.

--- src/me/eelek/advancedpvp/kits/Kit.java
+++ src/me/eelek/advancedpvp/kits/Kit.java
@@ -3,7 +3,10 @@ package me.eelek.advancedpvp.kits;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.potion.PotionEffect;
@@ -15,45 +18,85 @@ public class Kit {
 	private ArrayList<ItemStack> content;
 	private ArrayList<ItemStack> armor;
 	private ItemStack kitItem;
-	private ArrayList<PotionEffect> potionEffect;
+	private ArrayList<PotionEffect> potionEffects;
 	private int minimumLevel;
 	
+	/**
+	 * Kit Object.
+	 * @param name The name of the Kit.
+	 * @param author  The author of the Kit.
+	 * @param content A list containing all items that the Kit contains.
+	 * @param armor A list containing all the armor that the Kit contains.
+	 * @param kitItem The display item of the Kit.
+	 * @param minimumLevel The minimum level required for this Kit.
+	 */
 	public Kit(String name, String author, ArrayList<ItemStack> content, ArrayList<ItemStack> armor, ItemStack kitItem, int minimumLevel) {
 		this.name = name;
 		this.author = author;
 		this.content = content;
 		this.armor = armor;
 		this.kitItem = kitItem;
-		this.potionEffect = null;
+		this.potionEffects = null;
 		this.minimumLevel = minimumLevel;
 	}
 	
-	public Kit(String name, String author, ArrayList<ItemStack> content, ArrayList<ItemStack> armor, ItemStack kitItem, ArrayList<PotionEffect> potionEffect, int minimumLevel) {
+	/**
+	 * Kit Object.
+	 * @param name The name of the Kit.
+	 * @param author  The author of the Kit.
+	 * @param content A list containing all items that the Kit contains.
+	 * @param armor A list containing all the armor that the Kit contains.
+	 * @param kitItem The display item of the Kit.
+	 * @param potionEffects A list containing all potion effects associated with the Kit.
+	 * @param minimumLevel The minimum level required for this Kit.
+	 */
+	public Kit(String name, String author, ArrayList<ItemStack> content, ArrayList<ItemStack> armor, ItemStack kitItem, ArrayList<PotionEffect> potionEffects, int minimumLevel) {
 		this.name = name;
 		this.author = author;
 		this.content = content;
 		this.armor = armor;
 		this.kitItem = kitItem;
-		this.potionEffect = potionEffect;
+		this.potionEffects = potionEffects;
 		this.minimumLevel = minimumLevel;
 	}
 	
+	/**
+	 * Get the name of the Kit.
+	 * @return The name of the Kit.
+	 */
 	public String getName() {
 		return name;
 	}
 	
+	/**
+	 * Get a list of all the items in the Kit.
+	 * @return A list of all the items in the Kit.
+	 */
 	public ArrayList<ItemStack> getContent() {
 		return content;
 	}
 	
+	/**
+	 * Get an item by index.
+	 * @param index The index.
+	 * @return The item at the specified index.
+	 */
 	public ItemStack getItem(int index) {
 		return content.get(index);
 	}
 	
+	/**
+	 * Get the armor that the Kit contains.
+	 * @return A list with all the armor that the Kit contains.
+	 */
 	public ArrayList<ItemStack> getArmor() {
 		return armor;
 	}
 	
+	/**
+	 * Get the display item of the Kit.
+	 * @return The display item of the Kit.
+	 */
 	public ItemStack getDisplayItem() {
 		ItemMeta kMeta = (ItemMeta) kitItem.getItemMeta();
 		kMeta.setDisplayName(ChatColor.DARK_PURPLE + name);
@@ -62,20 +105,76 @@ public class Kit {
 		return kitItem;
 	}
 	
+	/**
+	 * Get the potion effects of the Kit.
+	 * @return The potion effects of the Kit.
+	 */
 	public ArrayList<PotionEffect> getPotionEffects() {
-		return potionEffect;
+		return potionEffects;
 	}
 	
+	/**
+	 * Get the minimum level required for this Kit.
+	 * @return The minimum level required for this Kit.
+	 */
 	public int getMinimumLevel() {
 		return minimumLevel;
 	}
 	
+	/**
+	 * Set the minimum level required for this Kit.
+	 * @param level The new minimum level required for this Kit.
+	 */
 	public void setMinimumLevel(int level) {
 		this.minimumLevel = level;
 	}
 	
+	/**
+	 * Get the author of the Kit.
+	 * @return The author of the Kit.
+	 */
 	public String getAuthor() {
 		return author;
 	}
+	
+	/**
+	 * Function that generates a preview inventory for the Kit.
+	 * @return A preview inventory for the Kit.
+	 */
+	public Inventory generatePreviewInventory(boolean showBack) {
+		int pageSize = (int) (18 + (Math.ceil((double) this.content.size() / 9) * 9));
+		Inventory inv = Bukkit.getServer().createInventory(null, pageSize, "[Kit] " + this.name);
+		
+		for (ItemStack i : this.armor) {
+			if (i.getType().toString().toLowerCase().contains("helmet")) {
+				inv.setItem(0, i);
+			} else if (i.getType().toString().toLowerCase().contains("chestplate") || i.getType().toString().toLowerCase().contains("elytra") || i.getType().toString().toLowerCase().contains("shield")) {
+				inv.setItem(1, i);
+			} else if (i.getType().toString().toLowerCase().contains("leggings")) {
+				inv.setItem(2, i);
+			} else if (i.getType().toString().toLowerCase().contains("boots")) {
+				inv.setItem(3,  i);
+			}
+		}
+		
+		for(int c = 0; c < this.content.size(); c++) {
+			if(c < 8) {
+				inv.setItem(pageSize - 9 + c, this.content.get(c));
+			} else {
+				inv.setItem(18 + c, this.content.get(this.content.size() - c - 1));
+			}
+		}
 
+		if(showBack) {
+			ItemStack back = new ItemStack(Material.REDSTONE_BLOCK, 1);
+			ItemMeta bMeta = (ItemMeta) back.getItemMeta();
+			bMeta.setDisplayName(ChatColor.RED + "Go back to the select menu.");;
+			back.setItemMeta(bMeta);
+			inv.setItem(8, back);	
+		} else {
+			inv.setItem(8, this.kitItem);
+		}
+
+		return inv;
+	}
 }
